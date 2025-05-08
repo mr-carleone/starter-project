984950d4c472b9c97d17c879e99dda0b2eb9025d
crive <iv.eruslanov@yandex.ru>
2025-05-08 18:17:30 +0300

    Add to shemas to auth token expiries

--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -1,5 +1,6 @@
 # src/schemas/__init__.py
 from .user_schema import UserCreate, UserInDB, RoleBase
 from .health_schema import HealthCheckResponse
+from .auth_schema import TokenResponse
 
-__all__ = ["UserCreate", "UserInDB", "RoleBase", "HealthCheckResponse"]
+__all__ = ["UserCreate", "UserInDB", "RoleBase", "HealthCheckResponse", "TokenResponse"]
